a3031ceeb0b26358df1176e928e34f5ab0aa0465
Barod55 <aljmalmhmwd03@gmail.com>
2026-01-31 00:57:13 +0200

    "add job views& url"

---
+++ job/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path , include
+from .import views
+
+urlpatterns = [
+    path('jobs/', views.job_list, name='job_list'),
+    path('<int:id>', views.job_detail, name='job_detail'),
+]
